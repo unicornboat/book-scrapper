09a6c25a2fca17cff31a9d9ac7658e13449a0c3b
UnicornBoat <unicornboat1994@gmail.com>
2020-12-04 02:59:13 +1000

    Working in progress
    - Fixed GBK decoding issue
    - Replace axios with http

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 const dotenv = require('dotenv').config();
-const axios = require('axios');
-const cheerio = require('cheerio');
+const fetcher = require('./lib/fetcher');
 const sites = require(__dirname + process.env.SITES_DATA);
 
 for (let key in sites) {
-	let site = sites[key];
-	site.categories.forEach(category => {
-		fetch(site.domain + category.uri, category.selector);
-	});
-}
-
-function fetch (url, selector) {
-	axios.get(url, {
-		headers: {
-			'User-Agent': process.env.USER_AGENT
-		}
-	}).then(res => {
-		const $ = cheerio.load(res.data);
-		console.log('Found %s element(s)', $(selector).length);
-		if ($(selector).length) {
-			$(selector).each((i, el) => {
-				console.log($(el).text());
-			});
-		}
-	}).catch(e => {
-		console.log(e);
-	});
+	fetcher.sync(sites[key]);
 }
